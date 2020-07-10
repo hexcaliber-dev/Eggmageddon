571a31c1f71225c8e6fc2a185a20219f80dfb20a
Epicrider <omarepic1@gmail.com>
2020-07-10 16:55:54 -0700

    Make Explosion Abstract Class

---
+++ Assets/Scripts/LaunchSystem/Explosion.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class Explosion : MonoBehaviour
+{
+
+    public float getX()
+    {
+        return transform.position.x;
+    }
+
+    public float getY()
+    {
+        return transform.position.y;
+    }
+    
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        applyAffect( collision.gameObject );
+    }
+
+    public abstract void applyAffect(GameObject unit);
+
+}
