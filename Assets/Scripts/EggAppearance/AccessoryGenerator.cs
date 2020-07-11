c7642591de9f7e14fca3b9720e873ac2026c961a
Epicrider <omarepic1@gmail.com>
2020-07-11 10:38:31 -0700

    Add new EggBois Scene and Add Random Hat Generation

---
+++ Assets/Scripts/EggAppearance/AccessoryGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AccessoryGenerator : MonoBehaviour
+{
+
+    public List<GameObject> hats;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int r = Random.Range(0, hats.Count);
+        if (r <= hats.Count-1) {
+            hats[r].SetActive(true);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
