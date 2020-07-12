6d3928ed09ffb4f77e9230e0ad3cf236098426f0
Eric Qian <eric@enumc.com>
2020-07-11 18:51:57 -0700

    fix: more elaborate launch countdown

--- Assets/Scripts/startArea.cs
+++ Assets/Scripts/startArea.cs
@@ -14,6 +14,7 @@ public class startArea : MonoBehaviour
     float currentTime;
     bool isEntering;
     string origText;
+    private List<Collider2D> colliders = new List<Collider2D>();
     // Start is called before the first frame update
     void Start()
     {
@@ -29,45 +30,54 @@ public class startArea : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Start area triggered");
+        // Debug.Log("Start area triggered");
         
-        isEntering = true;
+        // isEntering = true;
+        colliders.Add(other);
         StartCoroutine(startCountdown());
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        Debug.Log("Left start area");
-        isEntering = false;
-        startAreaText.text = origText;
+        // Debug.Log("Left start area");
+        // isEntering = false;
+        colliders.Remove(other);
+        if (colliders.Count == 0) {
+            startAreaText.text = origText;
+        }
+        
     }
 
     private IEnumerator startCountdown()
     {
-        if (isEntering) {
-            startAreaText.text="3";
+        if (colliders.Count != 0) {
+            startAreaText.text="T-3";
             yield return new WaitForSeconds(1);
-        }
-        
-        if (isEntering) {
-            startAreaText.text="2";
-            yield return new WaitForSeconds(1);
-        }
-
-        if (isEntering) {
-            startAreaText.text="1";
-            yield return new WaitForSeconds(1);
-        }
-
-        if (isEntering) {
-            if (transform.name == "StartArea")
-                SceneManager.LoadScene(firstScene.name, LoadSceneMode.Single);
-            else if (transform.name == "ExitArea") {
-                Debug.Log("exiting game");
-                Application.Quit(0);
+            if (colliders.Count != 0) {
+                startAreaText.text="T-2";
+                yield return new WaitForSeconds(1);
+                if (colliders.Count != 0) {
+                    startAreaText.text="T-1";
+                    yield return new WaitForSeconds(1);
+                    if (colliders.Count != 0) {
+                        if (transform.name == "StartArea")
+                            SceneManager.LoadScene(firstScene.name, LoadSceneMode.Single);
+                        else if (transform.name == "ExitArea") {
+                            Debug.Log("exiting game");
+                            Application.Quit(0);
+                        }
+                            
+                    }
+                }
             }
-                
         }
+
+        
+        
+
+        
+
+        
 
     }
 
